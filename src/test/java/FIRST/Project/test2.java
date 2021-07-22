ba82d2d9255e199d3d867d005ec962f07362a6bc
unknown <pallavi.jadhav2008@gmail.com>
2021-07-22 06:43:52 -0700

    second commit from original

---
+++ src/test/java/FIRST/Project/test2.java
@@ -0,0 +1,18 @@
+package FIRST.Project;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class test2 {
+
+	@Test
+	public void m3()
+	{SoftAssert sa=new SoftAssert();
+		System.out.println("m3");
+	}
+	@Test
+	public void m4()
+	{
+		System.out.println("m4");
+	}
+}
