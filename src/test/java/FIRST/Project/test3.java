12d2156daf1a525bcbffa37b51eaa3bc5eaec1a2
unknown <pallavi.jadhav2008@gmail.com>
2021-07-22 06:52:37 -0700

    commit from scnd workbench

---
+++ src/test/java/FIRST/Project/test3.java
@@ -0,0 +1,18 @@
+package FIRST.Project;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class test3 {
+
+	@Test
+	public void m5()
+	{SoftAssert sa=new SoftAssert();
+		System.out.println("m5");
+	}
+	@Test
+	public void m6()
+	{
+		System.out.println("m6");
+	}
+}
