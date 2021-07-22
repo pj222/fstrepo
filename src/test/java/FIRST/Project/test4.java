004a4cc5845c833cb4ce41d5bf1fc93e06e4f0ac
unknown <pallavi.jadhav2008@gmail.com>
2021-07-22 07:20:07 -0700

    fst commit from dev

---
+++ src/test/java/FIRST/Project/test4.java
@@ -0,0 +1,17 @@
+package FIRST.Project;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class test4 {
+	@Test
+	public void m7()
+	{SoftAssert sa=new SoftAssert();
+		System.out.println("m7");
+	}
+	@Test
+	public void m8()
+	{
+		System.out.println("m8");
+	}
+}
